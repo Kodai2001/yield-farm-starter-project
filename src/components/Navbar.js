6a313e8e94175b07be36c40406cadfe8af969124
Kodai Hayashi <hayashikdi0105@gmail.com>
2022-12-25 13:25:36 +0900

    UIを修正

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import farmer from '../farmer.png'
+import './App.css'
 
 class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="http://www.dappuniversity.com/bootcamp"
